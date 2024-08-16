bb9de5109cc5ac1ae1747a3f3a85ad7dc27a2703
Mark <markporcarojr@gmail.com>
2024-08-15 20:36:44 -0400

    Typo in last commit: Force Dynamic duplicates

--- app/page.tsx
+++ app/page.tsx
@@ -30,10 +30,6 @@ export default async function Home() {
 }
 export const dynamic = "force-dynamic";
 
-
-
-export const dynamic = "force-dynamic";
-
 export const metadata: Metadata = {
   title: "Issue Tracker - Dashboard",
   description: "View the summary of the project issues",
